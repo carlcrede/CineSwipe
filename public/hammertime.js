a4bab5a248ced84f0aa1c03d6a23306481627d6f
Christian <carl_hassel@hotmail.com>
2021-05-18 18:30:47 +0200

    swipe is now  handled as pan - added animation on swipe

--- public/hammertime.js
+++ public/hammertime.js
@@ -1,48 +1,61 @@
 let wrapper = $('.wrapper');
+const swipedElements = [];
 
 const initHammer = elem => {
     const hammertime = new Hammer(elem);
 
     hammertime.get('press').set({ time: 100 });
 
-    hammertime.get('swipe').set({ velocity: 0.9 });
+    hammertime.get('swipe').set({ velocity: 1.1 });
 
     hammertime.on('pan', (ev) => {
         elem.style.transition = '';
-        elem.style.transform = `translate3d(${ev.deltaX}px,0,0) rotateY(${ev.deltaX/1000}deg) rotateZ(${ev.deltaX/100}deg) scale(1.05)`;
+        elem.style.transform = `translate3d(${ev.deltaX}px, ${ev.deltaY}px, 0) rotateY(${ev.deltaX/1000}deg) rotateZ(${ev.deltaX/100}deg) scale(1.05)`;
     });
 
     hammertime.on('panstart', () => {
-        elem.style.transition = '.2s ease-in-out';
+        $(elem).toggleClass('moving', true);
         elem.style.transform = elem.style.transform.replace('scale(1)', 'scale(1.05)');
     });
 
     hammertime.on('panend', (ev) => {
-        elem.style.transition = '.2s ease-in-out';
-        elem.style.transform = `translate3d(0, 0, 0) scale(1)`;
+        $(elem).toggleClass('moving', false);
+        const moveOutWidth = document.body.clientWidth;
+        const keep = Math.abs(ev.velocityX) < 1.1;
+        if (!keep) {
+            const endX = Math.max(Math.abs(ev.velocityX) * moveOutWidth, moveOutWidth);
+            const toX = ev.deltaX > 0 ? endX : -endX;
+            const endY = Math.abs(ev.velocityY) * moveOutWidth;
+            const toY = ev.deltaY > 0 ? endY : -endY;
+            elem.style.transition = 'all .7s ease-in-out';
+            elem.style.transform = `translate3d(${toX}px, ${toY + ev.deltaY}px, 0)`;
+            swipedElements.push(elem);
+            addCard();
+            if (toX > 0) {
+                console.log('Liked movie id:', elem.id);
+                clientLikedMovie(elem.id);
+            } else {
+                console.log('Disliked movie id:', elem.id);
+            }
+        } else {
+            elem.style.transition = 'all .2s ease-in-out';
+            elem.style.transform = `translate3d(0, 0, 0) scale(1)`;
+        }
     });
 
     hammertime.on('press', () => {
-        elem.style.transition = '.2s ease-in-out';
-        //elem.style.transform = elem.style.transform.replace('scale(1)', 'scale(1.05)');
-        elem.style.transform = 'scale(1.05)';
+        $(elem).toggleClass('moving');
+        elem.style.transition = 'all .2s ease-in-out';
+        elem.style.transform = 'perspective(1px) scale(1.05)';
     });
 
     hammertime.on('pressup', () => {
+        $(elem).toggleClass('moving');
         elem.style.transform = elem.style.transform.replace('scale(1.05)', 'scale(1)');
     });
-
-    hammertime.on('swipe', () => {
-        elem.remove();
-        addCard();
-    });
-
-    hammertime.on('swiperight', (ev) => {
-        console.log('Liked movie id:', elem.id);
-        clientLikedMovie(elem.id);
-    });
-
-    hammertime.on('swipeleft', () => {
-        
-    });
+    
+    setTimeout(() => {
+        swipedElements.forEach(element => element.remove());
+    }, 1000);
 }
+
