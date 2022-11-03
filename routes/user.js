c04b007c6de67f3596d94b7c8cbace19f8dcc2cb
carlcrede <33420878+carlcrede@users.noreply.github.com>
2022-11-03 17:23:19 +0100

    route for getting user

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,25 @@
 const router = require('express').Router();
-
+const jwt = require('jsonwebtoken');
+const Users = require('../db/model/credentials.js');
 const Preferences = require('../db/model/preferences.js');
 
 const pages = require('../util/ssr');
+
+router.get('/me', async (req, res) => {
+    const token = req.headers.authorization.split(' ')[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (decoded.id) {
+        const user = await Users.findById(decoded.id, {password: 0});
+        if (user) {
+            res.send(user);
+        } else {
+            res.status(404).send('User not found');
+        }
+    } else {
+        res.status(401).send('Unauthorized');
+    }
+});
 
 router.get('/user/:id/preferences', (req, res, next) => {
     if (req.session.userId) {
