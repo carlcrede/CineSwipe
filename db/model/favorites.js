7211481c965f35225ba5415f847708cc421932f3
carlcrede <33420878+carlcrede@users.noreply.github.com>
2022-12-07 18:10:00 +0100

    favorites done

--- db/model/favorites.js
+++ db/model/favorites.js
@@ -5,6 +5,8 @@ const favoriteSchema = new Schema({
     user: { type: Schema.Types.ObjectId, ref: 'credentials' },
     media_type: { type: String, required: true },
     movieDbId: { type: String, required: true },
+    title: { type: String, required: true },
+    poster_path: { type: String, required: true },
 });
 
 module.exports = mongoose.model('favorite', favoriteSchema);
--- routes/user.js
+++ routes/user.js
@@ -76,7 +76,9 @@ router.post('/me/favorites', authenticateToken, async (req, res) => {
     const favorite = new Favorites({
         user: req.userId,
         media_type: body.media_type,
-        movieDbId: body.movieDbId
+        movieDbId: body.movieDbId,
+        title: body.title,
+        poster_path: body.poster_path,
     });
     favorite.save(err => {
         if (err) {
@@ -109,42 +111,6 @@ router.get('/me/favorites', authenticateToken, async (req, res) => {
         return res.status(404).send('Favorites not found');
     }
     res.send(favorites);
-});
-
-router.get('/likes', async (req, res) => {
-    const user = req.session.userId;
-    try {
-        if (user) {
-            const foundUser = await Preferences.findOne({ user: user });
-            const userLikes = foundUser.likes;
-            res.send(userLikes);
-        }
-    } catch (error) {
-        console.log(error);
-        res.status(500).send('Internal Server error occured');
-    }
-});
-
-router.post('/likes', async (req, res) => {
-    const like = req.body.like.id;
-    const media_type = req.body.like.media_type;
-    const genres = req.body.like.genres;
-    const user = req.session.userId;
-    try {
-        if (user) {
-            const filter = { user: user };
-            const update = { "$addToSet": { likes: [{ id: like, media_type: media_type, genres: genres }] } };
-            const doc = await Preferences.findOneAndUpdate(filter, update, {
-                new: true,
-                upsert: true
-            });
-            return res.send({ doc });
-        } else {
-            return res.send({});
-        }
-    } catch (error) {
-        res.status(500).send('Internal Server error occured');
-    }
 });
 
 module.exports = {
