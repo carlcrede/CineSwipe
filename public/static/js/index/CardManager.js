aa98f66f5d999f59cd39f28bc4e3885b60d93f2b
Crede <crede@handyhand.dk>
2025-03-29 18:35:08 +0100

    card when there are no more items

--- public/static/js/index/CardManager.js
+++ public/static/js/index/CardManager.js
@@ -1,125 +1,148 @@
 const CardManager = (() => {
+  let items = [];
+  const invalidItemStatuses = [
+    "Rumored",
+    "Planned",
+    "In Production",
+    "Post Production",
+    "Canceled",
+  ];
 
-    let items = [];
-    const invalidItemStatuses = ['Rumored', 'Planned', 'In Production', 'Post Production', 'Canceled'];
+  const updateCardsWithFilters = async (filters) => {
+    clearInterval(cardInterval);
+    $(".card-container").children(".card").not(".first").remove();
+    const newItems = await ItemFetch.fetchItems(filters);
+    items = newItems;
+    const cards = $(".card-container").children();
+    if (cards.length < 10 && items) {
+      for (let i = 0; i < 10 - cards.length; i++) {
+        addCard();
+      }
+    }
 
-    const updateCardsWithFilters = async (filters) => {
+    cardInterval = setInterval(checkAndRepopulate, 3000);
+  };
+
+  const addCardToMatches = async (item) => {
+    $("#matchesCount").text(++matches);
+    const details = await ItemFetch.fetchItemDetails(item);
+    let card = CardBuilder.buildItemCard(details, false);
+    card.css("position", "relative");
+    card.attr("id", `match-${item.id}`);
+    $(".match-container").prepend(card);
+    const match_notif = $("#match-notification");
+    match_notif.show();
+    setTimeout(() => {
+      match_notif.fadeOut(300);
+    }, 3000);
+  };
+
+  const addCard = () => {
+    if (items[0]) {
+      if (isValidItem(items[0])) {
+        let item = items[0];
+        let card = CardBuilder.buildItemCard(item);
+        initHammer(...card, item);
+        $(".card-container").prepend(card);
+        items.shift();
+      }
+    }
+  };
+
+  const isValidItem = (item) => {
+    if (item.status in invalidItemStatuses) {
+      return false;
+    } else if (item.media_type == "movie" && !item.title) {
+      return false;
+    } else if (item.media_type == "tv" && !item.name) {
+      return false;
+    }
+    return true;
+  };
+
+  const checkAndRepopulate = async () => {
+    if (document.hasFocus) {
+      if (items && items.length < 10) {
         clearInterval(cardInterval);
-        $('.card-container').children('.card').not('.first').remove();
-        const newItems = await ItemFetch.fetchItems(filters);
+        Filtering.paginate();
+        const newItems = await ItemFetch.fetchItems(Filtering.getFilters());
         items = newItems;
-        const cards = $('.card-container').children();
-        if(cards.length < 10 && items){
-            for(let i = 0; i < 10 - cards.length; i++){
-                addCard();
-            }
-        }
-        
         cardInterval = setInterval(checkAndRepopulate, 3000);
-    };
-
-    const addCardToMatches = async (item) => {
-        $('#matchesCount').text(++matches);
-        const details = await ItemFetch.fetchItemDetails(item);
-        let card = CardBuilder.buildItemCard(details, false);
-        card.css('position', 'relative');
-        card.attr('id', `match-${item.id}`);
-        $('.match-container').prepend(card);
-        const match_notif = $('#match-notification');
-        match_notif.show();
-        setTimeout(() => {
-            match_notif.fadeOut(300);
-        }, 3000);   
-    };
-
-    const addCard = () => {
-        if (items[0]) { 
-            if(isValidItem(items[0])){
-                let item = items[0];
-                let card = CardBuilder.buildItemCard(item);
-                initHammer(...card, item);
-                $('.card-container').prepend(card);
-                items.shift();
-            }
-        }
-    };
-
-    const isValidItem = (item) => {
-        if (item.status in invalidItemStatuses) { 
-            return false;
-        } else if (item.media_type == 'movie' && !item.title) {
-            return false;
-        } else if (item.media_type == 'tv' && !item.name) {
-            return false;
-        } 
-        return true;
-    };
-
-    const checkAndRepopulate = async() => {
-        if(document.hasFocus){
-            if(items && items.length < 10){
-                clearInterval(cardInterval);
-                Filtering.paginate();
-                const newItems = await ItemFetch.fetchItems(Filtering.getFilters());
-                items = newItems;
-                cardInterval = setInterval(checkAndRepopulate, 3000);
-            }
-            if (!items.length) {
-                console.log('out of items that match the filters in the current region.');
-                clearInterval(cardInterval);
-            }
-            const cards = $('.card-container').children();
-            if(cards.length < 10 && items){
-                for(let i = 0; i < 10 - cards.length; i++){
-                    addCard();
-                }
-            }
-        }
-    };
-
-    (function insertFirstCard() {
-        const firstCard = $('<div class="card first"></div>');
-
+      }
+      if (!items.length) {
+        console.log(
+          "out of items that match the filters in the current region."
+        );
+        // add a card that says "No more items to show"
+        const noMoreItems = $(
+          '<div class="card first"></div>'
+        );
         const instructions = $(
-            `<div class="instructions">
+          `<div class="instructions">
+                    <p>No more items to show 😭</p>
+                    <p>Try changing the filters or providers</p>
+                    </div>`
+        );
+        noMoreItems.append(instructions);
+        $(".card-container").append(noMoreItems);
+        clearInterval(cardInterval);
+      }
+      const cards = $(".card-container").children();
+      if (cards.length < 10 && items) {
+        for (let i = 0; i < 10 - cards.length; i++) {
+          addCard();
+        }
+      }
+    }
+  };
+
+  (function insertFirstCard() {
+    const firstCard = $('<div class="card first"></div>');
+
+    const instructions = $(
+      `<div class="instructions">
             <p>Swipe right or 👍 if you'd like to watch it.</p>
             <p>Swipe left or 👎 if you don't want to watch.</p>
             <p>Don't forget to invite your friends and family!</p>
-            </div>`);
-        
-        let btnDiv = $(`<div class="card-buttons"></div>`);
-        const likebtn = $(`<div class="begin" id="beginBtn">Begin swiping 👍</div>`);
-        btnDiv.append(likebtn);
-        const btnHammer = new Hammer(...likebtn);
+            </div>`
+    );
 
-        initHammer(...firstCard);
-        btnHammer.on('tap pressup', async (ev) => {
-            firstCard.css('transition', 'all .4s ease-in-out');
-            firstCard.css('transform', 'translate3d(2000px, 0, 0)');
-            addCard();
-            setTimeout(() => {
-                firstCard.remove();
-            }, 1000);
-        });
-        firstCard.append(instructions);
-        firstCard.append(btnDiv);
-        $('.card-container').append(firstCard);
-    })();
+    let btnDiv = $(`<div class="card-buttons"></div>`);
+    const likebtn = $(
+      `<div class="begin" id="beginBtn">Begin swiping 👍</div>`
+    );
+    btnDiv.append(likebtn);
+    const btnHammer = new Hammer(...likebtn);
 
-    (async function initCards(){
-        Filtering.initFilters();
-        const result = await ItemFetch.fetchInitialItems();
-        const initialItems = result;
-        items = initialItems;
-        for (let i = 0; i < 10; i++) {
-            addCard();
-        }
-    })();
+    initHammer(...firstCard);
+    btnHammer.on("tap pressup", async (ev) => {
+      firstCard.css("transition", "all .4s ease-in-out");
+      firstCard.css("transform", "translate3d(2000px, 0, 0)");
+      addCard();
+      setTimeout(() => {
+        firstCard.remove();
+      }, 1000);
+    });
+    firstCard.append(instructions);
+    firstCard.append(btnDiv);
+    $(".card-container").append(firstCard);
+  })();
 
-    let cardInterval = setInterval(checkAndRepopulate, 3000);
+  (async function initCards() {
+    Filtering.initFilters();
+    const result = await ItemFetch.fetchInitialItems();
+    const initialItems = result;
+    items = initialItems;
+    for (let i = 0; i < 10; i++) {
+      addCard();
+    }
+  })();
 
-    return {
-        addCard, addCardToMatches, updateCardsWithFilters
-    };
+  let cardInterval = setInterval(checkAndRepopulate, 3000);
 
+  return {
+    addCard,
+    addCardToMatches,
+    updateCardsWithFilters,
+  };
 })();
